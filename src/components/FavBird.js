aa143aff5a22a29810bb5c92b7b8d35209a5a3b3
Aaron Hubber <aaronhubber@gmail.com>
2022-05-14 18:49:12 +0100

    all done

--- src/components/FavBird.js
+++ src/components/FavBird.js
@@ -4,7 +4,9 @@ import React from "react"
 const FaveBird = ({bird}) => {
 
     return (
+        <>
         <p className="best-birds">In {bird.loc} a {bird.en}. It sounds like <audio controls><source src={bird.file}/></audio></p>
+        </>
     )
 }
 
